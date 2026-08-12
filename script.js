a6283fe67625dd8bac7f375b5c57795d5cf9933a
Phyu145 <psinthaw02@gmail.com>
2026-08-12 09:38:21 +0630

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const cakeBox = document.getElementById('cakeBox');
+const candle = document.getElementById('candle');
+const wishMessage = document.getElementById('wish-message');
+const instruction = document.getElementById('instruction');
+
+cakeBox.addEventListener('click', function() {
+  candle.classList.remove('hidden');
+  wishMessage.classList.remove('hidden');
+  instruction.innerText = " Happy Anniversary ";
+});
